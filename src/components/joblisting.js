d5534323d560f7b6562b11007bb226637952bb16
Will Negron <willnegron.dev@gmail.com>
2021-05-31 22:10:45 -0400

    got Like buttons working for multiple listings

--- src/components/joblisting.js
+++ src/components/joblisting.js
@@ -1,11 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 import ListingContainer from "./listingcontainer"
 import { listing } from "./joblisting.module.css"
 
 function JobListing() {
+
+    const details = [
+        {
+            title: "Manager  of Things", company: "Googel", salary: "$10,000", location: "Nueva York", time: "1 min ago", id: "1"
+        },
+        {
+            title: "Manager  of Things", company: "Googel", salary: "$10,000", location: "Nueva York", time: "1 min ago", id: "2"
+        }
+    ]
+
     return (
         <section className={listing}>
-            <ListingContainer title="Manager of Things" company="Googs" salary="100,000" location="New Yoek" time="1 min ago" />
+            {details.map(item => (
+                <ListingContainer title={item.title} company={item.company} salary={item.salary} location={item.location} time={item.time} idNumber={item.id} />
+            ))}
         </section>
     )
 }
